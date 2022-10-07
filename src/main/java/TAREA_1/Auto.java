f0b7f091b4c653545d3259c3c0a8b070031c8195
sebas <sebas@172.16.217.83>
2022-10-07 12:09:22 -0500

    FINAL

---
+++ src/main/java/TAREA_1/Auto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TAREA_1;
+
+/**
+ *
+ * @author sebas
+ */
+public class Auto {
+    String marca;
+    int ano;
+    String placa;
+    String color;
+}
