11b2ae27224132bba58bb7c7675e743f8cb3a40d
sebas <sebas@172.16.217.83>
2023-01-06 12:36:35 -0500

    Tarea.03.04(falta 2 menus)

--- src/main/java/TAREA_3_3/controlador/EquipoControl.java
+++ src/main/java/TAREA_3_3/controlador/EquipoControl.java
@@ -15,60 +15,59 @@ import java.util.List;
  * @author sebas
  */
 public class EquipoControl {
-    
-    public EquipoServiceImpl EquipoServiceImpl=new EquipoServiceImpl();
-    public PartidoServiceImpl PartidoServiceImpl=new PartidoServiceImpl();
-    
-    public void crear(String [] data){
-        
-        var codigo=Integer.valueOf(data[0]);
-        var nombre=data[1];
-        var pais=data[2];
-        var colorPrincipal=data[3];
-        var colorSecundario=data[4];
-        var partido=data[5];
-        var year=Integer.valueOf(data[6]).intValue();
-        var month=Integer.valueOf(data[7]).intValue();
-        var day=Integer.valueOf(data[8]).intValue();
-        var fechaFundacion = LocalDate.of(year,month, day);
-        var poblacion=data[9];
-       
-        var equipo=new Equipo(codigo,nombre,pais,colorPrincipal,colorSecundario
-                ,partido,LocalDate.of(year, month, day),poblacion);
+
+    public EquipoServiceImpl EquipoServiceImpl = new EquipoServiceImpl();
+    public PartidoServiceImpl PartidoServiceImpl = new PartidoServiceImpl();
+
+    public void crear(String[] data) {
+
+        var codigo = Integer.valueOf(data[0]);
+        var nombre = data[1];
+        var pais = data[2];
+        var colorPrincipal = data[3];
+        var colorSecundario = data[4];
+        var partido = data[5];
+        var year = Integer.valueOf(data[6]).intValue();
+        var month = Integer.valueOf(data[7]).intValue();
+        var day = Integer.valueOf(data[8]).intValue();
+        var fechaFundacion = LocalDate.of(year, month, day);
+        var poblacion = data[9];
+
+        var equipo = new Equipo(codigo, nombre, pais, colorPrincipal, colorSecundario,
+                 partido, LocalDate.of(year, month, day), poblacion);
         this.EquipoServiceImpl.crear(equipo);
     }
 
-     public String modificar(String[] data) {
-        var retorno="No se puede modificar";
-        var codigo=Integer.valueOf(data[0]);
-        var nombre=data[1];
-        var pais=data[2];
-        var colorPrincipal=data[3];
-        var colorSecundario=data[4];
-        var partido=data[5];
-        var year=Integer.valueOf(data[6]).intValue();
-        var month=Integer.valueOf(data[7]).intValue();
-        var day=Integer.valueOf(data[8]).intValue();
-        var fechaFundacion = LocalDate.of(year,month, day);
-        var poblacion=data[9];
+    public String modificar(String[] data) {
+        var retorno = "No se puede modificar";
+        var codigo = Integer.valueOf(data[0]);
+        var nombre = data[1];
+        var pais = data[2];
+        var colorPrincipal = data[3];
+        var colorSecundario = data[4];
+        var partido = data[5];
+        var year = Integer.valueOf(data[6]).intValue();
+        var month = Integer.valueOf(data[7]).intValue();
+        var day = Integer.valueOf(data[8]).intValue();
+        var fechaFundacion = LocalDate.of(year, month, day);
+        var poblacion = data[9];
         var modificar = Integer.valueOf(data[10]).intValue();
-        
-        var equipo=new Equipo(codigo,nombre,pais,colorPrincipal,colorSecundario
-                ,partido,LocalDate.of(year, month, day),poblacion);
+
+        var equipo = new Equipo(codigo, nombre, pais, colorPrincipal, colorSecundario,
+                 partido, LocalDate.of(year, month, day), poblacion);
         this.EquipoServiceImpl.modificar(equipo, modificar);
-        retorno="Modificado";
+        retorno = "Modificado";
         return retorno;
-     }
-    
-    
-    public List<Equipo> listar(){
-        return this.EquipoServiceImpl.listar();
     }
-    
+
     public void eliminar(String codigos) {
         var codigo = Integer.valueOf(codigos).intValue();
         this.EquipoServiceImpl.eliminar(codigo);
 
     }
-    
+
+    public List<Equipo> listar() {
+        return this.EquipoServiceImpl.listar();
+    }
+
 }
