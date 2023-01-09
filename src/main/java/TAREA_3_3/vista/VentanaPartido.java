c535099334f3a7a2516f3e299c8a3e3e256bcf91
sebas <sebas@172.16.217.83>
2023-01-09 12:37:50 -0500

    Tarea.03.05 FINAL

--- src/main/java/TAREA_3_3/vista/VentanaPartido.java
+++ src/main/java/TAREA_3_3/vista/VentanaPartido.java
@@ -5,12 +5,14 @@
 package TAREA_3_3.vista;
 
 import TAREA_3_3.controlador.PartidoControl;
+import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author sebas
  */
-public class VentanaPartido extends javax.swing.JFrame {
+public class VentanaPartido extends javax.swing.JInternalFrame {
 
     /**
      * Creates new form Ventana
@@ -18,6 +20,11 @@ public class VentanaPartido extends javax.swing.JFrame {
     private PartidoControl PartidoControl = new PartidoControl();
 
     public VentanaPartido() {
+        this.setClosable(true);
+        this.setIconifiable(true);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
+
         initComponents();
     }
 
@@ -47,7 +54,7 @@ public class VentanaPartido extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("codigo");
 
@@ -115,43 +122,42 @@ public class VentanaPartido extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel4)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(14, 14, 14)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jLabel4)
+                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(69, 69, 69)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 818, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(14, 14, 14)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(159, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(146, 146, 146)
+                        .addGap(284, 284, 284)
                         .addComponent(jButton1)
                         .addGap(18, 18, 18)
                         .addComponent(jButton2)
                         .addGap(18, 18, 18)
                         .addComponent(jButton3)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton4))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(69, 69, 69)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 460, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(jButton4)))
+                .addContainerGap(97, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -181,27 +187,48 @@ public class VentanaPartido extends javax.swing.JFrame {
                     .addComponent(jButton3)
                     .addComponent(jButton4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 291, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(34, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        var data = new String[6];
-        data[0] = this.jTextField1.getText();
-        data[1] = this.jTextField2.getText();
-        data[2] = this.jTextField3.getText();
-        data[3] = this.jTextField4.getText();
-        data[4] = this.jTextField5.getText();
-        data[5] = this.jTextField6.getText();
+        if (JOptionPane.showConfirmDialog(this, "Seguro de guardar datos?",
+                 "Sistema de equipos", JOptionPane.ERROR_MESSAGE,
+                 JOptionPane.QUESTION_MESSAGE) == 0) {
+            var data = new String[6];
+            data[0] = this.jTextField1.getText();
+            data[1] = this.jTextField2.getText();
+            data[2] = this.jTextField3.getText();
+            data[3] = this.jTextField4.getText();
+            data[4] = this.jTextField5.getText();
+            data[5] = this.jTextField6.getText();
 
-        this.PartidoControl.crear(data);
+            this.PartidoControl.crear(data);
+            JOptionPane.showMessageDialog(this, "Se guardaron los datos", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+
+        } else {
+
+            JOptionPane.showMessageDialog(this, "No se guardaron los datos", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        this.actualizarTabla();
+        if (JOptionPane.showConfirmDialog(this, "Seguro de listar los datos?",
+                 "Sistema de equipos", JOptionPane.ERROR_MESSAGE,
+                 JOptionPane.QUESTION_MESSAGE) == 0) {
+
+            this.actualizarTabla();
+            JOptionPane.showMessageDialog(this, "Se listaron los datos",
+                     "Aviso", JOptionPane.INFORMATION_MESSAGE);
+
+        } else {
+
+            JOptionPane.showMessageDialog(this, "No se listar los datos",
+                     "Aviso", JOptionPane.INFORMATION_MESSAGE);
+        }
     }//GEN-LAST:event_jButton2ActionPerformed
     public void actualizarTabla() {
 
@@ -209,8 +236,8 @@ public class VentanaPartido extends javax.swing.JFrame {
         for (var i = 0; i < this.PartidoControl.listar().size(); i++) {
 
             data[i][0] = this.PartidoControl.listar().get(i).getCodigo();
-            data[i][1] = this.PartidoControl.listar().get(i).getEquipo1().getNombre();
-            data[i][2] = this.PartidoControl.listar().get(i).getEquipo2().getNombre();
+            data[i][1] = this.PartidoControl.listar().get(i).getEquipo1();
+            data[i][2] = this.PartidoControl.listar().get(i).getEquipo2();
             data[i][3] = this.PartidoControl.listar().get(i).getFechaPartido().toString();
 
         }
@@ -224,57 +251,62 @@ public class VentanaPartido extends javax.swing.JFrame {
         jTable1.setModel(new javax.swing.table.DefaultTableModel(data, encabezado));
     }
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        var data = new String[6];
-        data[0] = this.jTextField1.getText();
-        data[1] = this.jTextField2.getText();
-        data[2] = this.jTextField3.getText();
-        data[3] = this.jTextField4.getText();
-        data[4] = this.jTextField5.getText();
-        data[5] = this.jTextField6.getText();
+        if (JOptionPane.showConfirmDialog(this, "Seguro de modificar datos?",
+                 "Sistema de equipos", JOptionPane.ERROR_MESSAGE,
+                 JOptionPane.QUESTION_MESSAGE) == 0) {
 
-        this.PartidoControl.modificar(data);
+            var finaliza = false;
+            while (finaliza == false) {
+
+                try {
+                    String[] data = new String[7];
+                    data[0] = this.jTextField1.getText();
+                    data[1] = this.jTextField2.getText();
+                    data[2] = this.jTextField3.getText();
+                    data[3] = this.jTextField4.getText();
+                    data[4] = this.jTextField5.getText();
+                    data[5] = this.jTextField6.getText();
+                    data[6] = this.jTextField1.getText();
+
+                    this.PartidoControl.modificar(data);
+                    JOptionPane.showMessageDialog(this, "Datos modificados",
+                             "Aviso", JOptionPane.INFORMATION_MESSAGE);
+                    finaliza = true;
+                } catch (NumberFormatException el) {
+                    JOptionPane.showMessageDialog(this, "Debe ingresar un numero",
+                             "Aviso", JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+
+        } else {
+
+            JOptionPane.showMessageDialog(this, "No se modificaron los datos",
+                     "Aviso", JOptionPane.INFORMATION_MESSAGE);
+        }
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        if (JOptionPane.showConfirmDialog(this, "¿Seguro de eliminar los datos?",
+                 "Sistema de equipos", JOptionPane.ERROR_MESSAGE,
+                 JOptionPane.QUESTION_MESSAGE) == 0) {
         var codigo = this.jTextField1.getText();
         this.PartidoControl.eliminar(codigo);
+        this.actualizarTabla();
+
+            JOptionPane.showMessageDialog(this, "Se eliminaron los datos",
+                     "Aviso", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+
+            JOptionPane.showMessageDialog(this, "No se eliminaron los datos",
+                     "Aviso", JOptionPane.INFORMATION_MESSAGE);
+        }
     }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VentanaPartido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VentanaPartido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VentanaPartido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VentanaPartido.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VentanaPartido().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
